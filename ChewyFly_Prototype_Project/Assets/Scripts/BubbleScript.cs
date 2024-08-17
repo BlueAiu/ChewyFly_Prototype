1f0a089ae6d3f82c21670edd07d7301793c379d1
BlueAiu <aiujustitia@yahoo.co.jp>
2024-08-17 14:53:30 +0900

    泡の当たり判定を出すまでに時間がかかるようにした
    小さな泡が出て時間が経ったらドーナツを反発させるようにした

--- ChewyFly_Prototype_Project/Assets/Scripts/BubbleScript.cs
+++ ChewyFly_Prototype_Project/Assets/Scripts/BubbleScript.cs
@@ -6,22 +6,35 @@ public class BubbleScript : MonoBehaviour
 {
     [Tooltip("小さな泡エフェクト")]
     [SerializeField] GameObject smallBubble;
-    [Tooltip("滞在する時間")]
+    [Tooltip("弾き判定を出し始める時間")]
+    [SerializeField] float bubbleStartTime = 0.5f;
+    [Tooltip("弾き判定が滞在する時間")]
     [SerializeField] float lifeTimeMin = 5f;
     [SerializeField] float lifeTimeMax = 10f;
     [Tooltip("ドーナツを弾き飛ばす力")]
     [SerializeField] float donutBoundPower = 5f;
 
+    Collider collider;
+
     // Start is called before the first frame update
     void Start()
     {
-        Invoke(nameof(Delete), Random.Range(lifeTimeMin, lifeTimeMax));
+        collider = GetComponent<Collider>();
+        collider.enabled = false;
+
+        Invoke(nameof(BoundStart), bubbleStartTime);
+        Invoke(nameof(Delete), bubbleStartTime + Random.Range(lifeTimeMin, lifeTimeMax));
     }
 
     // Update is called once per frame
     void Update()
     {
         ;
+    }
+
+    void BoundStart()
+    {
+        collider.enabled = true;
     }
 
     void Delete()

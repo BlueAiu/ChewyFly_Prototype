e3450cb9b8aae21e9c8d347fe64301809379f45e
arukapega <dragon.night3579@gmail.com>
2024-10-07 04:50:41 +0900

    普通のドーナツ完成時のエフェクトを新しく分けて、ドーナツ完成時エフェクトが発生するようにしました

--- ChewyFly_Prototype_Project/Assets/Scripts/Donut/DonutsUnionScript.cs
+++ ChewyFly_Prototype_Project/Assets/Scripts/Donut/DonutsUnionScript.cs
@@ -160,4 +160,13 @@ public partial class DonutsUnionScript : MonoBehaviour
             sphere.transform.GetChild(0).gameObject.SetActive(active);
         }
     }
+    public Vector3 GetDonutsCenterPoint()
+    {
+        Vector3 center = Vector3.zero;
+        foreach(var sphere in donutSpheres)
+        {
+            center += sphere.transform.position;
+        }
+        return center /= donutSpheres.Count;
+    }
 }
